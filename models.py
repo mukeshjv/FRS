78c5216a71529c1dfe6bf2569045b8b003c815cf
Mukesh <mukesh.fdmje@sihgad.edu>
2021-04-06 21:25:34 +0530

    Removed redundant files

--- models.py
+++
@@ -1,7 +0,0 @@
-class Property():
-    def __init__(self, recipe_id, name, minutes,  ingredients, n_ingredients):
-      self.id = recipe_id
-      self.name = name
-      self.minutes = minutes
-      self.ingredients = ingredients
-      self.n_ingredients = n_ingredients
